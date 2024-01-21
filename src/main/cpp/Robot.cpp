c12d4b99aaf3c62ac2226ed2e5505ed8b3503eb6
mimi980 <90112270+mimi980@users.noreply.github.com>
2024-01-21 09:45:30 +0100

    edit

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -43,134 +43,45 @@ void Robot::AutonomousPeriodic()
 
   NLTRJ_POSTED_MESSAGE message; // Posted Message
 
-  // switch (m_state)
-  // {
-  // case Robot::STATE::PATH_ERROR:
-  //   break;
+  switch (m_state)
+  {
+  case Robot::STATE::PATH_ERROR:
+    break;
 
-  // case Robot::STATE::PATH_FOLLOWING:
-  //   // *****************************************************    'THE' METHOD(e)
-  //   // A) Feed back:
-  //   // avec les encodeurs on estime la position du robot:
-  //   //			l = distance parcourue par la roue gauche depuis le dernier reset encodeur.
-  //   //			r = distance parcourue par la roue droite depuis le dernier reset encodeur.
-  //   //
-  //   //			dl et dr = distances parcourues par les roues gauche et droite depuis le dernier call.
-  //   //			(note dl/dt = vitesse roue gauche et dr/dt = vitesse roue droite  )
-  //   //
+  case Robot::STATE::PATH_FOLLOWING:
+    // *****************************************************    'THE' METHOD(e)
+    // A) Feed back:
+    // avec les encodeurs on estime la position du robot:
+    //			l = distance parcourue par la roue gauche depuis le dernier reset encodeur.
+    //			r = distance parcourue par la roue droite depuis le dernier reset encodeur.
+    //
+    //			dl et dr = distances parcourues par les roues gauche et droite depuis le dernier call.
+    //			(note dl/dt = vitesse roue gauche et dr/dt = vitesse roue droite  )
+    //
 
-  //   m_follower.estimate(m_robotContainer.m_drivetrain.m_EncoderLeft.GetDistance(), m_robotContainer.m_drivetrain.m_EncoderRight.GetDistance(), NDEGtoRAD(m_gyro.GetAngle()));
-  //   m_follower.updateTarget(&m_TrajectoryPack, 0.02f);
-  //   pout = m_follower.compute();
-  //   m_robotContainer.m_drivetrain.SetVoltage(m_CrtzR.getVoltage(pout->m_rightVelocity, pout->m_rightAcceleration), m_CrtzL.getVoltage(pout->m_leftVelocity, pout->m_leftAcceleration));
+    m_follower.estimate(m_robotContainer.m_drivetrain.m_EncoderLeft.GetDistance(), m_robotContainer.m_drivetrain.m_EncoderRight.GetDistance(), NDEGtoRAD(m_gyro.GetAngle()));
+    m_follower.updateTarget(&m_TrajectoryPack, 0.02f);
+    pout = m_follower.compute();
+    m_robotContainer.m_drivetrain.SetVoltage(m_CrtzR.getVoltage(pout->m_rightVelocity, pout->m_rightAcceleration), m_CrtzL.getVoltage(pout->m_leftVelocity, pout->m_leftAcceleration));
 
-  //   while (m_follower.getMessage(&message))
-  //   {
-  //     switch (message.m_id)
-  //     {
-  //     case OPEN_INTAKE:
-  //       m_robotContainer.m_intake.Open();
-  //       break;
-  //     case CLOSE_INTAKE:
-  //       m_robotContainer.m_intake.Close();
-  //       break;
-  //     case ACTIVATE_INTAKE_WHEELS_TAKE:
-  //       m_robotContainer.m_intake.SetSpeed(0.6);
-  //       break;
-  //     case ACTIVATE_INTAKE_WHEELS_DROPOFF:
-  //       m_robotContainer.m_intake.SetSpeed(-0.6);
-  //       break;
-  //     case DEACTIVATE_INTAKE_WHEELS:
-  //       m_robotContainer.m_intake.SetSpeed(0.0);
-  //       break;
-  //     case ACTIVATE_CONVEYOR_TAKE:
-  //       m_robotContainer.m_conveyor.SetSpeed(0.6);
-  //       break;
-  //     case ACTIVATE_CONVEYOR_DROPOFF:
-  //       m_robotContainer.m_conveyor.SetSpeed(-0.6);
-  //       break;
-  //     case DEACTIVATE_CONVEYOR:
-  //       m_robotContainer.m_conveyor.SetSpeed(0.0);
-  //       break;
-  //     case TURRET_0:
-  //       m_robotContainer.m_turret.SetSetpoint(0.0);
-  //       break;
-  //     case TURRET_L90:
-  //       m_robotContainer.m_turret.SetSetpoint(-90.0);
-  //       break;
-  //     case TURRET_R90:
-  //       m_robotContainer.m_turret.SetSetpoint(90.0);
-  //       break;
-  //     case TURRET_L180:
-  //       m_robotContainer.m_turret.SetSetpoint(-180.0);
-  //       break;
-  //     case TURRET_R180:
-  //       m_robotContainer.m_turret.SetSetpoint(180.0);
-  //       break;
-  //     case GRIPPER_TAKE_CUBE:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_gripper.Take(0.6);
-  //       break;
-  //     case GRIPPER_TAKE_CONE:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_gripper.Take(0.6);
-  //       break;
-  //     case GRIPPER_DROP_CUBE_OFF:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_gripper.Spit();
-  //       break;
-  //     case GRIPPER_DROP_CONE_OFF:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_gripper.Spit();
-  //       break;
-  //     case ARM_TO_LEVEL_CONE_1:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_elevator.SetSetpoint(0.72);
-  //       m_robotContainer.m_arm.SetSetpoint(NDEGtoRAD(105.0));
-  //       m_robotContainer.m_gripper.DropMidleCone = true;
-  //       break;
-  //     case ARM_TO_LEVEL_CONE_2:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_elevator.SetSetpoint(0.97);
-  //       m_robotContainer.m_arm.SetSetpoint(NDEGtoRAD(120.0));
-  //       break;
-  //     case ARM_TO_LEVEL_CUBE_1:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_elevator.SetSetpoint(0.43);
-  //       m_robotContainer.m_arm.SetSetpoint(NDEGtoRAD(97.0));
-  //       m_robotContainer.m_gripper.DropHighCube = true;
-  //       break;
-  //     case ARM_TO_LEVEL_CUBE_2:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_elevator.SetSetpoint(0.98);
-  //       m_robotContainer.m_arm.SetSetpoint(NDEGtoRAD(115.0));
-  //       m_robotContainer.m_gripper.DropHighCube = true;
-  //       break;
-  //     case ARM_HOME:
-  //       m_takeCubeInRobot = false;
-  //       m_robotContainer.m_elevator.SetSetpoint(0.0);
-  //       m_robotContainer.m_arm.SetSetpoint(NDEGtoRAD(100.0));
-  //       break;
-  //     case TAKE_CUBE_IN_ROBOT:
-  //       m_takeCubeInRobot = true;
-  //       m_StateTakeCubeRobot = StateTakeCubeRobot::Init;
-  //       break;
-      
-  //     default:
-  //       break;
-  //     }
-  //   }
+    while (m_follower.getMessage(&message))
+    {
+      switch (message.m_id)
+      {
+      default:
+        break;
+      }
+    }
 
-  //   break;
+    break;
 
-  // case Robot::STATE::PATH_END:
-  //   m_autoCube = true;
+  case Robot::STATE::PATH_END:
 
-  //   break;
-  // default:
-  //   NErrorIf(1, NERROR_UNAUTHORIZED_CASE);
-  //   break;
-  // }
+    break;
+  default:
+    NErrorIf(1, NERROR_UNAUTHORIZED_CASE);
+    break;
+  }
   
 }
 
