4cf7f47194507699ca9da5513c6f04751a20437d
mimi980 <90112270+mimi980@users.noreply.github.com>
2024-02-26 21:55:49 +0100

    c'est tout propre

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -24,7 +24,7 @@ void Robot::TeleopInit()
   m_MotorRight.SetInverted(false);
   m_MotorLeft.SetInverted(false);
   m_FeederMotor.SetInverted(true);
-  m_Pivot.SetInverted(true);
+  // m_Pivot.SetInverted(true);
 
   m_MotorLeft.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
   m_MotorRight.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
@@ -42,14 +42,14 @@ void Robot::TeleopInit()
   m_MotorLeft.ConfigVoltageCompSaturation(12);
   m_FeederMotor.ConfigVoltageCompSaturation(12);
 
-  frc::SmartDashboard::PutNumber("speedShooter", 0.65);
-  frc::SmartDashboard::PutNumber("speedAspiration", -0.2);
-  frc::SmartDashboard::PutNumber("speedCatch", 0.6);
-  frc::SmartDashboard::PutNumber("coef", 0.04);
+  frc::SmartDashboard::PutNumber("speedShooter", SPEED_SHOOTER);
+  frc::SmartDashboard::PutNumber("speedAspiration", SPEED_ASPIRATION);
+  frc::SmartDashboard::PutNumber("speedCatch", SPEED_CATCH);
+  frc::SmartDashboard::PutNumber("Goals", GOALS);
 
   m_count = 0;
   m_state = State::End;
-
+  /*
   m_pid.Reset();
   m_encoder.SetDistancePerPulse(((1.0 / 2048.0) / 4.5) * 360.0);
 
@@ -61,69 +61,25 @@ void Robot::TeleopInit()
 
   m_csv.setItem(0, "current", 5, &m_current);
   m_csv.setItem(1, "speed", 5, &m_speed);
-  m_csv.setItem(2, "encoder", 5, &m_position);
+  m_csv.setItem(2, "encoder", 5, &m_position);*/
 }
 void Robot::TeleopPeriodic()
 {
-  ///////CSV//////
-  m_csv.write();
-  m_current = m_Pivot.GetOutputCurrent();
-
-  //////pid///////////////////////////////////////////
-  m_position = -m_encoder.GetDistance();
-  m_speed = m_pid.Calculate(m_position);
-  m_pid.SetSetpoint(-m_Jostick_Right.GetY() * 180.0);
-  frc::SmartDashboard::PutNumber("setpoint", -m_Jostick_Right.GetY() * 180.0);
-  frc::SmartDashboard::PutNumber("encoder", m_position);
-  frc::SmartDashboard::PutNumber("speed", m_speed);
-  m_pid.SetGains(frc::SmartDashboard::GetNumber("P", 0.0), frc::SmartDashboard::GetNumber("I", 0.0), frc::SmartDashboard::GetNumber("D", 0.0));
-  m_Pivot.Set(NCLAMP(-0.9, m_speed, 0.9));
-  frc::SmartDashboard::PutNumber("current", m_current);
-
-  ////////////////////////////////////////////////////////////////////////
-
-  m_coeff = frc::SmartDashboard::GetNumber("coef", 0.04);
-
-  m_speedShootHigh = (m_speedShoot * 1.5 - (0.5 * m_speedCatch * 0.26)) / 1.5 - m_coeff;
-  // m_speedShootHigh = m_speedShoot + 0.06;
-
-  m_speedShoot = frc::SmartDashboard::GetNumber("speedShooter", 0.65);
-  frc::SmartDashboard::PutNumber("HighspeedShooter", m_speedShootHigh);
-  m_speedAspiration = frc::SmartDashboard::GetNumber("speedAspiration", -0.2);
-  m_speedCatch = frc::SmartDashboard::GetNumber("speedCatch", 0.6);
+  m_EncoderShooter = ((m_MotorLeft.GetSensorCollection().GetIntegratedSensorVelocity() * 600.0 / 2048.0) + (m_MotorRight.GetSensorCollection().GetIntegratedSensorVelocity() * 600.0 / 2048.0)) / 2.0;
+  m_speedAspiration = frc::SmartDashboard::GetNumber("speedAspiration", SPEED_ASPIRATION);
+  m_speedCatch = frc::SmartDashboard::GetNumber("speedCatch", SPEED_CATCH);
+  m_speedShoot = frc::SmartDashboard::GetNumber("speedShooter", SPEED_SHOOTER);
+  m_goals = frc::SmartDashboard::GetNumber("Goals", GOALS);
   frc::SmartDashboard::PutBoolean("Sensor", m_infraSensor.Get());
-
-  if (m_Jostick_Right.GetRawButton(1)) // shoot
-  {
-    m_MotorLeft.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedShoot);
-    m_MotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedShoot);
-    if (m_Jostick_Right.GetRawButton(2))
-    {
-      m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedCatch);
-    }
-  }
-  // else if (m_Jostick_Right.GetRawButton(4))
-  // {
-  //   m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedCatch);
-  //   if (!m_infraSensor.Get())
-  //   {
-  //     m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
-  //   }
-  // }
-  else if (m_Jostick_Right.GetRawButtonPressed(3))
-  {
-    m_state = State::Catch;
-    m_count = 0;
-  }
-
-  else
-  {
-
-    m_MotorLeft.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
-    m_MotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
-  }
+  frc::SmartDashboard::PutNumber("Encoder", m_EncoderShooter);
 
   m_count++;
+
+  if (m_Jostick_Right.GetRawButtonPressed(3))
+  {
+    m_state = State::End;
+  }
+
   switch (m_state)
   {
   case State::Catch:
@@ -131,62 +87,82 @@ void Robot::TeleopPeriodic()
     if (!m_infraSensor.Get())
     {
       m_count = 0;
-      m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.4);
+      m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedAspiration);
       m_state = State::Recul;
     }
     break;
   case State::Recul:
-    m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.2);
+    m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedAspiration);
     if (m_infraSensor.Get())
     {
       m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+      m_state = State::Loaded;
+    }
+    break;
+  case State::Loaded:
+    m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+    if (m_Jostick_Right.GetRawButtonPressed(1))
+    {
+      m_state = State::PreShoot;
+    }
+    break;
+  case State::PreShoot:
+    m_MotorLeft.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedShoot);
+    m_MotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedShoot);
+    if (m_EncoderShooter > m_goals)
+    {
+      m_state = State::Shoot;
+    }
+    break;
+  case State::Shoot:
+    m_MotorLeft.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedShoot);
+    m_MotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedShoot);
+    m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedCatch);
+
+    if (m_infraSensor.Get())
+    {
       m_state = State::End;
     }
     break;
   case State::End:
+    m_MotorLeft.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+    m_MotorRight.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+    m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
+    if (m_Jostick_Right.GetRawButtonPressed(2))
+    {
+      m_state = State::Catch;
+    }
     break;
+
   default:
     break;
   }
-  if (m_Jostick_Right.GetRawButtonPressed(4))
-  {
-    m_state = State::End;
-  }
 
-  // if (m_Jostick_Right.GetRawButtonPressed(3))
-  // {
-  //   m_state = State::Catch;
-  //   m_count = 0;
-  // }
-  // m_count++;
-  // switch (m_state)
-  // {
-  // case State::Catch:
-  //   m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, m_speedCatch);
-  //   if (!m_infraSensor.Get())
-  //   {
-  //     m_count = 0;
-  //     m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.2);
-  //     m_state = State::Recul;
-  //   }
-  //   break;
-  // case State::Recul:
-  //   m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.2);
-  //   if (m_infraSensor.Get())
-  //   {
-  //     m_FeederMotor.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.0);
-  //   }
-  //   break;
-  // case State::End:
-  //   break;
-  // default:
-  //   break;
-  // }
+  // m_speedShootHigh = (m_speedShoot * 1.5 - (0.5 * m_speedCatch * 0.26)) / 1.5 - m_coeff;
+  // frc::SmartDashboard::PutNumber("HighspeedShooter", m_speedShootHigh);
+
+  // ############################################################################################################
+  // ///////CSV//////
+  // m_csv.write();
+  // m_current = m_Pivot.GetOutputCurrent();
+
+  // //////pid///////////////////////////////////////////
+  // m_position = -m_encoder.GetDistance();
+  // m_speed = m_pid.Calculate(m_position);
+  // m_pid.SetSetpoint(-m_Jostick_Right.GetY() * 180.0);
+  // frc::SmartDashboard::PutNumber("setpoint", -m_Jostick_Right.GetY() * 180.0);
+  // frc::SmartDashboard::PutNumber("encoder", m_position);
+  // frc::SmartDashboard::PutNumber("speed", m_speed);
+  // m_pid.SetGains(frc::SmartDashboard::GetNumber("P", 0.0), frc::SmartDashboard::GetNumber("I", 0.0), frc::SmartDashboard::GetNumber("D", 0.0));
+  // m_Pivot.Set(NCLAMP(-0.9, m_speed, 0.9));
+  // frc::SmartDashboard::PutNumber("current", m_current);
+
+  // ////////////////////////////////////////////////////////////////////////
 }
 
 void Robot::DisabledInit()
 {
-  m_csv.close();
+  // m_csv.close();
 }
 void Robot::DisabledPeriodic() {}
 
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <iostream>
 #include <frc/TimedRobot.h>
 #include <ctre/Phoenix/motorcontrol/can/TalonFX.h>
 #include <rev/CANSparkMax.h>
@@ -14,6 +15,11 @@
 #include <frc/DigitalInput.h>
 #include "NLCsv.h"
 #include "RblUtils.h"
+
+#define SPEED_SHOOTER 0.65
+#define SPEED_ASPIRATION -0.2
+#define SPEED_CATCH 0.6
+#define GOALS 5000
 
 class Robot : public frc::TimedRobot
 {
@@ -41,25 +47,17 @@ private:
   ctre::phoenix::motorcontrol::can::TalonFX m_MotorLeft{2};
   ctre::phoenix::motorcontrol::can::TalonFX m_FeederMotor{3};
 
-  rev::CANSparkMax m_Pivot{4, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
-
-  Pid m_pid{0.0, 0.0, 0.0, 0.0};
-  frc::Encoder m_encoder{0, 1};
-
   frc::Joystick m_Jostick_Left{0};
   frc::Joystick m_Jostick_Right{1};
 
   frc::DigitalInput m_infraSensor{2};
 
-  double m_position;
-  double m_speed;
-  double m_current;
-  double m_speedShootHigh;
   double m_speedShoot;
   double m_speedAspiration;
   double m_speedCatch;
-  double m_coeff;
   int m_count;
+  double m_EncoderShooter;
+  double m_goals;
 
   enum State
   {
@@ -72,6 +70,13 @@ private:
   };
 
   State m_state;
-
+  /*
+  double m_position;
+  double m_speed;
+  double m_current;
   NLCSV m_csv{3};
+  rev::CANSparkMax m_Pivot{4, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  Pid m_pid{0.0, 0.0, 0.0, 0.0};
+  frc::Encoder m_encoder{0, 1};
+  */
 };
